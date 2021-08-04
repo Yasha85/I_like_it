19b5a3af865da584e33393d8e14de85c599d3b00
Alex Yanushevski <Yasha85@gmail.com>
2021-08-04 21:27:57 +0300

    Task_1

---
+++ CoolModule/Controller/Message/Index.php
@@ -0,0 +1,10 @@
+<?php
+namespace Amasty\CoolModule\Controller\Message;
+class Index extends \Magento\Framework\App\Action\Action
+{
+    public function execute()
+    {
+        echo 'Привет Amasty! Готов и буду изучать Magento 2. Пока, вроде, получается)))';
+        exit();
+    }
+}
